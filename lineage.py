f1520bb70b7871c22127126aca2e9d8b2a728eae
Dylan Nguyen <dylan@Dylans-MacBook-Air.local>
2026-01-28 00:43:04 +1100

    Add new class docstring

--- lineage.py
+++ lineage.py
@@ -1,6 +1,6 @@
 import sys
 import dataclasses
-from typing import Dict, List
+from typing import Dict, List, Self
 from sqlglot.optimizer.qualify import qualify
 from sqlglot.optimizer.scope import Scope, build_scope
 from sqlglot import parse_one
@@ -11,32 +11,66 @@ import logging
 logger = logging.getLogger(__name__)
 
 class Node:
+    """
+    Base Node class for Table and Column
+    Storing data related upstream and downstream
+
+    """
     def __init__(self, name: str):
         self.name = name
         self.secondName = None
-        self.upstream = []
-        self.downstream = []
+        self.upstream: List[Node] = []
+        self.downstream: List[Node] = []
 
-    def add_downstream(self, node):
+    def add_downstream(self, node:Self):
+        """
+        Adding other node into the downstream list
+        Static method -> return nothing
+
+        :param node: The downstream node
+        :return: None -> static method
+        """
         self.downstream.append(node)
         node.upstream.append(self)
 
-    def add_upstream(self, node):
+    def add_upstream(self, node: Self):
+        """
+        Adding the node (parent) into upstream
+        Add the current node into parent's downstream list
+
+        :param node: parent node
+        :return: None -> static method
+        """
         self.upstream.append(node)
         node.downstream.append(self)
 
     def detach_node(self):
+        """
+        Detatch the current node from its parents' downstream and children' upstream list
+
+        :return:
+        """
         for node in self.upstream:
-            if node in self.downstream:
+            if self in node.downstream:
                 node.downstream.remove(self)
 
         for node in self.downstream:
-            if node in self.upstream:
+            if self in node.upstream:
                 node.upstream.remove(self)
 
         return
 
 class ColumnNode(Node):
+    """
+    Base Column node based on Node class
+
+    table: The table node that the column belongs to
+    type: The column type (if there is any)
+    alias: The column alias
+    column_sources: List of tuple (table_name, column_name) indicate where the table originated from inside a given query
+    table_identifier: The table identifier (if there is any)
+
+    """
     def __init__(self, name: str, table_identifier: str = None, alias: str = None, column_sources = None):
         super().__init__(name)
         self.table: TableNode | None = None
@@ -44,8 +78,6 @@ class ColumnNode(Node):
         self.alias = alias
         self.column_sources = column_sources
         self.table_identifier = table_identifier
-
-
 
     def __str__(self):
         return f"Column {self.name}"
@@ -59,16 +91,24 @@ class ColumnNode(Node):
     def detach_column(self):
         self.detach_node()
 
-
-
 class TableNode(Node):
-    def __init__(self, name: str, scope: Scope | exp.Expression, schema = None):
+    """
+    Table node for Scope/Table/Create Expression
+
+    columns: Dict of ColumnNode -> name of the column as keys and ColumnNode as values
+    schema: The table schema name (if there is any)
+    scope: The Scope/Table/Create Expression that the table belongs to
+    sources: Dict of TableNode that the current Node derived value from -> name of the table as keys and TableNode as values
+    table_column_mapping: Mapping of source table as key and pair of (source column name, ColumnNode) as value -> used mainly for connecting columns connections between parent and child tables
+    """
+    def __init__(self, name: str, scope: Scope | exp.Expression, schema = None, db = None):
         super().__init__(name)
         self.columns: Dict[str, ColumnNode] = {}
         self.schema = schema
         self.scope: Scope | exp.Expression | None = scope
         self.sources = {}
         self.table_column_mapping = {}
+
 
         self.direct_upstream: ColumnNode | None = None
 
@@ -81,6 +121,7 @@ class TableNode(Node):
 
     def add_column(self, column: ColumnNode):
         self.columns[column.name] = column
+        column.table = self
 
     def connect_direct_upstream(self, upstream):
         self.direct_upstream = upstream
@@ -148,9 +189,7 @@ class LineageMap:
         if scope in self.visitedScopes:
             return self.visitedScopes[scope]
 
-        if isinstance(scope, exp.Table):
-            name = scope.name
-        elif not name:
+        if not name:
             name = self.generate_name("Scope")
 
         root = TableNode(name, scope=scope)
@@ -163,7 +202,6 @@ class LineageMap:
             for col in scope.expression.selects:
                 column = self._parse_column(col, root)
                 root.add_column(column)
-                column.table = root
 
                 for t_source, c_source in column.get_source_column_name():
                     if t_source not in root.table_column_mapping:
@@ -172,15 +210,27 @@ class LineageMap:
                         root.table_column_mapping[t_source].append([c_source, column])
 
             for name, source in scope.sources.items():
-                childtable = self._parse_scope(source, name)
+                if isinstance(source, exp.Table):
+                    child_table = self._parse_table(source)
+                else:
+                    child_table = self._parse_scope(source, name)
 
-                root.sources[name] = childtable
-                root.downstream.append(childtable)
-                childtable.upstream.append(root)
+                root.sources[name] = child_table
+                root.add_downstream(child_table)
 
             self._connect_nodes(root)
 
         return root
+
+    def _parse_table(self, table: exp.Table)-> TableNode:
+        name = table.name
+        db = table.db
+        catalog = table.catalog
+
+        if db:
+            name = db + "." +  name
+
+        return TableNode(name, scope=table, schema=db, db=catalog)
 
 
     def _connect_nodes(self, root: TableNode)-> None:
@@ -207,6 +257,8 @@ class LineageMap:
         parent_node = self.visitedScopes[parent]
         node.connect_direct_upstream(parent_node)
 
+
+
     def _parse_create_table(self, create: exp.Create):
         """
         Parsing table or create expression create table node with relatives columns
@@ -214,6 +266,7 @@ class LineageMap:
         :param table:
         :return:
         """
+
         if create in self.visitedScopes:
             return self.visitedScopes[create]
 
@@ -221,11 +274,10 @@ class LineageMap:
         if not table:
             return None
 
-        root = TableNode(table.name, scope = create, schema=table.db)
+        root = self._parse_table(table)
         for column in create.find_all(exp.ColumnDef):
             col = ColumnNode(column.name, alias=column.alias)
             root.add_column(col)
-            col.root = table
 
         return root
 
@@ -250,7 +302,7 @@ class LineageMap:
         old.detach_table()
         self.tableNodeMap[old.name] = node
 
-    def connect_node_column(self, current_node: TableNode, down_stream_node:TableNode):
+    def connect_node_column(self, current_node: TableNode, down_stream_node: TableNode):
         for col in current_node.columns.values():
             if col.name in down_stream_node.columns:
                 col.add_downstream(down_stream_node.columns[col.name])
